19a9ec151909e05b03b4057878242dd62500d5ab
ernuan <36343710+ernuan@users.noreply.github.com>
2022-04-24 23:54:45 +0800

    Add files via upload

---
+++ js/ver.js
@@ -0,0 +1,16 @@
+console.log(btjson_ver);
+
+if(btjson_ver == "1.0.7"){
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/btjson1.0.7.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/btjsonplayer1.0.7.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/layer.min.js'></script>");
+}
+if(btjson_ver == "1.0.6"){
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/md5.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/jquery.min.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/btjson1.0.6.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/btjsonplayer1.0.6.js'></script>");
+	document.write("<script language=javascript src='https://cdn2.6dm.xyz/js/js/layer.min.js'></script>");
+}
